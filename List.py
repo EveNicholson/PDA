a4e1ea2ec9947ea2558b9202229ff6766ad0c178
Ewelina Nicholson <e.guzikk@gmail.com>
2023-07-14 16:34:57 +0100

    Created function

--- List.py
+++ List.py
@@ -1,2 +1,11 @@
 employees = ["Ewelina", "Philip", "Marta", "Jarek"]
 
+
+def employee_name(employee):
+    if employee == "Marta":
+        return True
+    else:
+        return False
+    
+print(employee_name("Marta"))
+    
