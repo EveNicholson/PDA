ea08dcbd11a48e3d6238fa2273469771801a30ee
Ewelina Nicholson <e.guzikk@gmail.com>
2023-07-14 17:23:28 +0100

    Created dictionary of friends

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+friends = {
+    "Dorota": {"country": "Poland",
+                "job": "Sale assistant",
+                "age": 37},
+    "Krzysztof": {"country": "Poland",
+                  "job": "tiler",
+                  "age": 45},
+    "Cameron": {"country": "Scotland",
+                "job": "Police officer",
+                "age": 26}            
+                }
