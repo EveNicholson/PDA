34505a5e40952d6ff69c0f220956fdb450a9d358
Ewelina Nicholson <e.guzikk@gmail.com>
2023-07-14 16:57:28 +0100

    Comments

--- List.py
+++ List.py
@@ -9,3 +9,10 @@ def employee_name(employee):
     
 print(employee_name("Marta"))
     
+
+
+# Lists in programing are used to collect similar items in a single variable
+# so they are easy to read and understand. 
+
+# The code I used is checking if name "Marta" is on the list of employees.
+
